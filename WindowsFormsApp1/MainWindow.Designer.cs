498f176538b50fdc95c6cfb44b16ede410b6c5ef
AQAD Abdel <48525287+aqadabdel@users.noreply.github.com>
2019-03-14 10:12:27 +0100

    Add files via upload

---
+++ WindowsFormsApp1/MainWindow.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace PressureBar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbl_srport = new System.Windows.Forms.Label();
+            this.cb_port = new System.Windows.Forms.ComboBox();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.btn_start = new System.Windows.Forms.Button();
+            this.btn_stop = new System.Windows.Forms.Button();
+            this.cb_interval = new System.Windows.Forms.ComboBox();
+            this.lbl_interval = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lbl_exp = new System.Windows.Forms.Label();
+            this.lbl_mbar = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lbl_srport
+            // 
+            this.lbl_srport.AutoSize = true;
+            this.lbl_srport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_srport.Location = new System.Drawing.Point(29, 30);
+            this.lbl_srport.Name = "lbl_srport";
+            this.lbl_srport.Size = new System.Drawing.Size(73, 16);
+            this.lbl_srport.TabIndex = 0;
+            this.lbl_srport.Text = "Serial Port:";
+            // 
+            // cb_port
+            // 
+            this.cb_port.FormattingEnabled = true;
+            this.cb_port.Location = new System.Drawing.Point(110, 29);
+            this.cb_port.Name = "cb_port";
+            this.cb_port.Size = new System.Drawing.Size(71, 21);
+            this.cb_port.TabIndex = 1;
+            this.cb_port.SelectedIndexChanged += new System.EventHandler(this.cb_port_SelectedIndexChanged);
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("OpenSymbol", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.Location = new System.Drawing.Point(70, 25);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(262, 79);
+            this.lbl_time.TabIndex = 2;
+            this.lbl_time.Text = "00:00:00";
+            // 
+            // btn_start
+            // 
+            this.btn_start.Location = new System.Drawing.Point(15, 255);
+            this.btn_start.Name = "btn_start";
+            this.btn_start.Size = new System.Drawing.Size(190, 39);
+            this.btn_start.TabIndex = 3;
+            this.btn_start.Text = "Start";
+            this.btn_start.UseVisualStyleBackColor = true;
+            this.btn_start.Click += new System.EventHandler(this.btn_start_Click);
+            // 
+            // btn_stop
+            // 
+            this.btn_stop.Location = new System.Drawing.Point(217, 255);
+            this.btn_stop.Name = "btn_stop";
+            this.btn_stop.Size = new System.Drawing.Size(189, 39);
+            this.btn_stop.TabIndex = 4;
+            this.btn_stop.Text = "Stop";
+            this.btn_stop.UseVisualStyleBackColor = true;
+            this.btn_stop.Click += new System.EventHandler(this.btn_stop_Click);
+            // 
+            // cb_interval
+            // 
+            this.cb_interval.FormattingEnabled = true;
+            this.cb_interval.Location = new System.Drawing.Point(110, 64);
+            this.cb_interval.Name = "cb_interval";
+            this.cb_interval.Size = new System.Drawing.Size(71, 21);
+            this.cb_interval.TabIndex = 6;
+            this.cb_interval.SelectedIndexChanged += new System.EventHandler(this.cb_interval_SelectedIndexChanged);
+            // 
+            // lbl_interval
+            // 
+            this.lbl_interval.AutoSize = true;
+            this.lbl_interval.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_interval.Location = new System.Drawing.Point(29, 65);
+            this.lbl_interval.Name = "lbl_interval";
+            this.lbl_interval.Size = new System.Drawing.Size(72, 16);
+            this.lbl_interval.TabIndex = 5;
+            this.lbl_interval.Text = "Interval (s):";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lbl_srport);
+            this.groupBox1.Controls.Add(this.cb_port);
+            this.groupBox1.Controls.Add(this.cb_interval);
+            this.groupBox1.Controls.Add(this.lbl_interval);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(190, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Configuration";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lbl_exp);
+            this.groupBox2.Controls.Add(this.lbl_mbar);
+            this.groupBox2.Location = new System.Drawing.Point(212, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(194, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pressure mBar";
+            // 
+            // lbl_exp
+            // 
+            this.lbl_exp.AutoSize = true;
+            this.lbl_exp.Location = new System.Drawing.Point(146, 19);
+            this.lbl_exp.Name = "lbl_exp";
+            this.lbl_exp.Size = new System.Drawing.Size(0, 13);
+            this.lbl_exp.TabIndex = 4;
+            // 
+            // lbl_mbar
+            // 
+            this.lbl_mbar.AutoSize = true;
+            this.lbl_mbar.Font = new System.Drawing.Font("OpenSymbol", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mbar.Location = new System.Drawing.Point(6, 16);
+            this.lbl_mbar.Name = "lbl_mbar";
+            this.lbl_mbar.Size = new System.Drawing.Size(162, 79);
+            this.lbl_mbar.TabIndex = 3;
+            this.lbl_mbar.Text = "0000";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lbl_time);
+            this.groupBox3.Location = new System.Drawing.Point(15, 118);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(391, 121);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Time:";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(170, 305);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
+            this.statusStrip1.Size = new System.Drawing.Size(247, 22);
+            this.statusStrip1.TabIndex = 11;
+            this.statusStrip1.Text = "statusBar";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(230, 17);
+            this.toolStripStatusLabel1.Text = "  PressureBar by AQAD Abdel -MP (c) 2018";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.BottomRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(418, 327);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_stop);
+            this.Controls.Add(this.btn_start);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PressureBar 0.4  Z:\\Desktop\\PRESSURE_OUTPUT\\";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_srport;
+        private System.Windows.Forms.ComboBox cb_port;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Button btn_start;
+        private System.Windows.Forms.Button btn_stop;
+        private System.Windows.Forms.ComboBox cb_interval;
+        private System.Windows.Forms.Label lbl_interval;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lbl_mbar;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Label lbl_exp;
+    }
+}
+
---
+++ WindowsFormsApp1/MainWindow.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+using System.Diagnostics;
+using System.Timers;
+using System.Globalization;
+
+namespace PressureBar
+{
+    public partial class Form1 : Form
+    {
+        System.Windows.Forms.Timer timr;
+        Stopwatch sw;
+        string[] ports = SerialPort.GetPortNames();
+        int interval = 1;
+        SerialPort sp = new SerialPort();
+        System.IO.StreamWriter csv;
+        StringBuilder outputLines = new StringBuilder();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
+
+            if (ports.Count() > 0)
+            {
+                foreach (string port in ports)
+                {
+                    cb_port.Items.Add(port);
+                    Console.WriteLine(port);
+                }
+                cb_port.SelectedText = ports[0];
+                sp.BaudRate = 9600;
+                sp.PortName = ports[0];
+                sp.DtrEnable = true;
+
+            }
+            int[] intervals = { 1, 2, 5, 10, 15 };
+
+            foreach (int interval in intervals)
+            {
+                cb_interval.Items.Add(interval);
+            }
+            cb_interval.SelectedText = intervals[0].ToString();
+
+
+        }
+
+
+        private void btn_start_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                sp.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Le port " + sp.PortName.ToString() + " ne peut pas être ouvert.\n" +
+                    " Veuillez choisir le bon port série.");
+                Console.WriteLine("Error opening serial port " + ex.ToString());
+                return;
+            }
+            lbl_time.ForeColor = System.Drawing.Color.Black;
+            string userpath = System.Environment.GetEnvironmentVariable("USERPROFILE") + "\\Desktop\\";
+
+            timr = new System.Windows.Forms.Timer();
+            timr.Interval = (1000);
+            timr.Tick += new EventHandler(timer_Tick);
+            sw = new Stopwatch();
+            timr.Start();
+            sw.Start();
+
+            btn_start.Text = "Started";
+            btn_stop.Text = "Stop";
+
+            lbl_time.Text = String.Format("{0:hh\\:mm\\:ss}", sw.Elapsed);
+
+            // System.IO.Directory.CreateDirectory("z:\\Desktop\\OUTPUT_PRESSURE");
+            System.IO.Directory.CreateDirectory(userpath + "OUTPUT_PRESSURE");
+            DateTime date_now = DateTime.Now;
+            string suffix = DateTime.Now.ToString("dd_MM_yyyy__hh_mm_ss");
+            csv = new System.IO.StreamWriter(userpath + "OUTPUT_PRESSURE\\PRESSURE_" + suffix + ".csv");
+            outputLines.AppendLine("\"TEMPS\";\"VALEUR MBAR\"");
+            //csv.WriteLine("\"TEMPS\";\"VALEUR MBAR\"");
+
+        }
+
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            lbl_time.Text = String.Format("{0:hh\\:mm\\:ss}", sw.Elapsed);
+
+
+            if (Math.Round(sw.Elapsed.TotalSeconds) % interval == 0)
+            {
+                string mbar = sp.ReadLine();
+                if (mbar == "")
+                {
+                    Console.WriteLine("INVALID SERIAL INPUT");
+                }
+                else
+                {
+                    //string mbar = "0, 3.6000E-03 mbar";
+                    string raw_value = mbar.Substring(3, 10);
+                    string value = mbar.Substring(3, 6);
+                    float val = float.Parse(value, CultureInfo.InvariantCulture);
+
+                    int exp = int.Parse(mbar.Substring(10, 3));
+
+                    decimal mbar_value = (int)(val * Math.Pow(10, exp)); ;
+
+                    if (exp <= 0)
+                    {
+                        mbar_value = (decimal)val;
+                        mbar_value = decimal.Round(mbar_value, 2);
+                        if (exp < 0)
+                        {
+                            lbl_exp.Text = "x10^" + exp.ToString();
+                        }
+                    }
+                    else
+                    {
+                        lbl_exp.Text = "";
+                    }
+
+                    //    Console.WriteLine("VAL: " + raw_value);
+                    //  Console.WriteLine("MBAR: " + mbar_value.ToString());
+                    lbl_mbar.Text = mbar_value.ToString();
+                    string formated_time = String.Format("{0:hh\\:mm\\:ss}", sw.Elapsed);
+                    //  Console.Write(" " + formated_time + "    " + mbar_value + " mBar RAW: " + raw_value + " mbar");
+                    outputLines.AppendLine("\"" + formated_time + "\";\"" + raw_value + "\"");
+                    //csv.WriteLine("\"" + formated_time + "\";\"" + raw_value + "\"");
+                }
+            }
+
+            Application.DoEvents();
+        }
+
+        private void btn_stop_Click(object sender, EventArgs e)
+        {
+            lbl_time.ForeColor = System.Drawing.Color.OrangeRed;
+            btn_start.Text = "Start";
+            btn_stop.Text = "Stopped";
+
+            try
+            {
+                csv.Write(outputLines.ToString());
+                timr.Stop();
+                sw.Stop();
+                sp.Close();
+                csv.Close();
+                csv.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error closing serial port " + ex.ToString());
+                return;
+            }
+
+
+        }
+
+        private void cb_interval_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            interval = int.Parse(cb_interval.Text);
+            Console.WriteLine(interval.ToString());
+        }
+
+        private void cb_port_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            sp.PortName = cb_port.Text;
+        }
+
+
+    }
+}
